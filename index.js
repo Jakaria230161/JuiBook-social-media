53fd45ee32c08b362bee98c652f24cbc1d3d4fdb
Jakaria230161 <jakariamahmud016@gmail.com>
2022-12-29 19:54:14 +0600

    font size added

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const messages = document.querySelector('.messages');
 const message = messages.querySelectorAll('.message');
 const messageSearch = document.querySelector('#message-search');
 
+//Theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+
 // ==================Sidebar===============
 // Remove active class from all menu items
 const changeActiveItem = () => {
@@ -54,4 +60,63 @@ messagesNotification.addEventListener('click', () => {
   setTimeout(() => {
     messages.style.boxShadow = 'none';
   }, 2000);
+})
+
+// Theme customizations
+
+// Opens Modal
+const openThemeModal = () => {
+  themeModal.style.display = 'grid';
+}
+
+// Closes Modal
+const closeThemeModal = (e) => {
+  if (e.target.classList.contains('customize-theme')) {
+    themeModal.style.display = 'none';
+  }
+}
+
+//Close Modal
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
+
+
+// ============fonts==============
+fontSizes.forEach(size => {
+  let fontSize;
+
+  size.addEventListener('click', () => {
+    
+  if (size.classList.contains('font-size-1')) {
+    fontSize = '10px';
+    root.style.setProperty('--sticky-top-left', '5.4rem');
+    root.style.setProperty('--sticky-top-right', '5.4rem');
+  }
+  else if (size.classList.contains('font-size-2')) {
+    fontSize = '13px';
+    root.style.setProperty('--sticky-top-left', '5.4rem');
+    root.style.setProperty('--sticky-top-right', '-7rem');
+  }
+  else if (size.classList.contains('font-size-3')) {
+    fontSize = '16px';
+    root.style.setProperty('--sticky-top-left', '-2rem');
+    root.style.setProperty('--sticky-top-right', '-17rem');
+  }
+  else if (size.classList.contains('font-size-4')) {
+    fontSize = '19px';
+    root.style.setProperty('--sticky-top-left', '-5rem');
+    root.style.setProperty('--sticky-top-right', '-25rem');
+  }
+  else if (size.classList.contains('font-size-5')) {
+    fontSize = '22px';
+    root.style.setProperty('--sticky-top-left', '-12rem');
+    root.style.setProperty('--sticky-top-right', '-35rem');
+    }
+    
+    // change the font size of the root html element
+    document.querySelector('html').style.fontSize = fontSize;
+    
+  })
+  
 })
